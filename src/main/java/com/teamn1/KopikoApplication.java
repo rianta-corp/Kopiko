91a5275b35ff5c4ba1ac835242f55f1cdc8cfbdb
rianta9 <hoangquockhanh122@gmail.com>
2021-03-10 13:45:19 +0700

    first commit create and config project

---
+++ src/main/java/com/teamn1/KopikoApplication.java
@@ -0,0 +1,13 @@
+package com.teamn1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopikoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopikoApplication.class, args);
+	}
+
+}
