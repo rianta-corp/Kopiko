cb9c3d1b0fe635f8d8193066e41ad01a4941ade9
rianta9 <hoangquockhanh122@gmail.com>
2021-03-12 16:11:58 +0700

    update home controller

--- src/main/java/com/kopiko/controller/web/HomeController.java
+++ src/main/java/com/kopiko/controller/web/HomeController.java
@@ -1,8 +1,12 @@
 package com.kopiko.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+	@RequestMapping(value = {"/", "home"})
+	public String home() {
+		return "home";
+	}
 }
