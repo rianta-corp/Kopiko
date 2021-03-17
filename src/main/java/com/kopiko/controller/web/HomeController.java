91c24b3600e14f7f67c2f10ddc13b6cf25d284f8
trung <nstrung163@gmail>
2021-03-17 15:48:51 +0700

    Test demo home page

--- src/main/java/com/kopiko/controller/web/HomeController.java
+++ src/main/java/com/kopiko/controller/web/HomeController.java
@@ -3,8 +3,6 @@ package com.kopiko.controller.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kopiko.entity.ProductImage;
-
 @Controller
 public class HomeController {
 	
