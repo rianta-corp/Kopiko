454d90867d08abe203fb5cc957aaa8591b49499a
rianta9 <hoangquockhanh122@gmail.com>
2021-03-26 11:22:05 +0700

    update payment feature/fix some layout problem

--- src/main/java/com/kopiko/dto/AccountDTO.java
+++ src/main/java/com/kopiko/dto/AccountDTO.java
@@ -3,14 +3,22 @@
  */
 package com.kopiko.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author rianta9
  * @datecreated 15 thg 3, 2021 13:26:34
  */
+
+@Data
+@NoArgsConstructor
 public class AccountDTO {
 	private Long accountId;
 	private String username;
 	private String fullName;
+	private String phone;
+	private String address;
 	private String avatar;
 	private String role;
 }
